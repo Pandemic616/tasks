91f5891e27bd1a5a638e83f224ca30f1a41b6c57
Pandemic616 <graviton5000@gmail.com>
2022-04-13 21:29:02 +0500

    начало пути )

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # первый комит
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# x = 0
+# for i in lst:
+#     x += i
+# print(x)
+# import random
+#
+# lst = [x for x in range(5,30)]
+# random.shuffle(lst)
+# m=lst[0]
+# n=lst[0]
+# for i in lst:
+#     if i < m:
+#         m = i
+#     elif i > n:
+#         n = i
+# print(m, n)
