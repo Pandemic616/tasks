f10c69f8c2af206534c8078e9c38f746a4fbd600
Pandemic616 <graviton5000@gmail.com>
2022-04-14 10:55:56 +0500

    начало пути )

--- main.py
+++ main.py
@@ -16,3 +16,40 @@
 #     elif i > n:
 #         n = i
 # print(m, n)
+# def calc_cube_perimeter(side):
+#     return side * 12
+#
+# def calc_cube_area(side):
+#     one_face = side * side
+#     cube_area = one_face * 6
+#     return cube_area
+#
+# def calc_cube(side, amount):
+#     one_cube_perimeter = calc_cube_perimeter(side)
+#     full_length = one_cube_perimeter * amount
+#     one_cube_area = calc_cube_area(side)
+#     full_area = one_cube_area * amount
+#     print('Для', amount, 'кубов понадобится палок (м):', full_length, 'и стекла (кв.м):', full_area)
+#
+# calc_cube(int(input()), int(input()))
+
+# # задача из яндекс практикума
+# def get_together_games(games_1, games_2):
+#     return set(games_1).intersection(set(games_2))
+#
+# anfisa_games = [
+#     'Online-chess',
+#     'Города',
+#     'DOOM',
+#     'Крестики-нолики'
+# ]
+# alisa_games = [
+#     'DOOM',
+#     'Online-chess',
+#     'Города',
+#     'GTA',
+#     'World of tanks'
+# ]
+# together_games = get_together_games(anfisa_games, alisa_games)
+# for game in together_games:
+#     print('👾', game)
