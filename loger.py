da0f15653ab27c892ada4532cb727ca5046e0140
Pandemic616 <graviton5000@gmail.com>
2022-04-30 13:55:44 +0500

    Fix

--- loger.py
+++ loger.py
@@ -1,34 +1,37 @@
-# значит есть функция которой на вход идут данные  x, y
-# функция делит эти числа
-# функция вызывается несколько раз( один раз идет вызов с параметрами 1, 0)
-# нужен декоратор который в случае ошибки будет ее обрабатывать и записывать в файл эту ошибку
-# фаил должен быть один, должен пополнятся
+"""
+начит есть функция которой на вход идут данные  x, y
+функция делит эти числа
+функция вызывается несколько раз( один раз идет вызов с параметрами 1, 0)
+нужен декоратор который в случае ошибки будет ее обрабатывать и записывать в файл эту ошибку
+фаил должен быть один, должен пополнятся
+"""
+from functools import wraps
+from typing import Callable
 
 
-def log_error(func):
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except Exception as err:
-                bag_file = open("Log_ERROR.txt", "a", encoding="UTF-8")
-                bag_file.write(f'произошла ошибка {err}\n')
-                bag_file.close()
-                return f'произошла ошибка {err}'
+def log_error(func: Callable):
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Callable | str:
+        try:
+            return func(*args, **kwargs)
+        except Exception as err:
+            bag_file = open("Log_ERROR.txt", "a", encoding="UTF-8")
+            bag_file.write(f'произошла ошибка {err}\n')
+            bag_file.close()
+            return (
+                f'произошла ошибка {err}: '
+                f'funcname={func.__name__}, '
+                f'funcmodule={func.__module__}, '
+                f'funcdoc={func.__doc__}'
+            )
 
-        return wrapper
-
-
-
-
-
-
-
-
+    return wrapper
 
 
 @log_error
-def division(x, y):
-    return x//y
+def division(x: int, y: int) -> float:
+    """Производит деление чисел"""
+    return x / y
 
 
 division(1, 1)
